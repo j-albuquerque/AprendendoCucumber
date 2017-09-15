f7d248d2a27a345be14d67e8c9d28732a1a47bcd
peo_jaoliveira <peo_jaoliveira@uolinc.com>
2017-09-15 17:53:20 -0300

    Colocando novos exemplos

--- features/Testando.feature
+++ features/Testando.feature
@@ -1,14 +1,23 @@
+# encoding: utf-8
 # language: pt
 
 Funcionalidade: Aprendendo a usar o Cucumber com Ruby
-    Acesso a páginas do google para realizar pesquisa
+    Acesso a páginas do google para realizar pesquisa e login no gemail
 
 Cenario: Pesquisar Queijo
     Dado que eu acesse o "http://www.google.com.br"
     Quando pesquisar "Queijo"
     Então o titulo da página deve ser "Queijo - Pesquisa Google"
 
-Cenario: Pesquisar Carne
+Cenario: Acessar Gmail
     Dado que eu acesse o "http://www.google.com.br"
-    Quando pesquisar "Carnee"
-    Então o titulo da página deve ser "Carne - Pesquisa Google"
+    Quando selecionar a opção Gmail
+    Então o titulo da página deve ser "Gmail"
+
+Cenario: Enviar E-mail
+    Dado que eu acesse o "http://www.google.com.br"
+    Quando selecionar a opção Gmail
+    E logar na conta de e-mail
+    E selecionar a opção Escrever
+    E preencher destinatário, assunto e corpo do e-mail
+    Entao deve exibir uma mensagem de enviado ao enviar o e-mail
--- features/step_definitions/Testando.rb
+++ features/step_definitions/Testando.rb
@@ -9,7 +9,8 @@ end
 
 #Insere um dado no campo de busca e realiza a busca
 Quando (/^pesquisar "(.*)"$/) do |pesquisa| 
-    element = @browser.find_element :name => "q"
+    #element = @browser.find_element :name => "q"
+    element = @browser.find_element(:name, "q")
     element.send_keys(pesquisa)
     element.send_keys :enter
 end
@@ -19,4 +20,57 @@ end
 Entao (/^o titulo da página deve ser "(.*)"$/) do |titlepage| 
     titulo = @browser.title
     expect(titulo).to eq(titlepage)
+    @browser.close
+end
+
+Quando (/^selecionar a opção Gmail$/) do
+    #Encrontra a opção Gmail e faz o click
+    element = @browser.find_element(:link, "Gmail")
+    element.click
+end
+
+E (/^logar na conta de e-mail$/) do
+    #Usuário senha invalidos, deve ser alterado antes de rodar o teste
+    user = "teste@gmail.com"
+    pass = "teste2017"
+    
+    #Insere o usuário e clica em "Próxima" para fazer o login 
+    #e espera 2 segundos para realizar a próxima ação
+    usuario = @browser.find_element(:name, "identifier")
+    usuario.send_keys(user)
+    @browser.find_element(:css, "span.RveJvd.snByac").click
+    sleep 2
+
+    #Insere a senha e clica em "Próxima" para fazer o login
+    senha = @browser.find_element(:name, "password")
+    senha.send_keys(pass)
+    @browser.find_element(:css, "span.RveJvd.snByac").click
+end
+
+E (/^selecionar a opção Escrever$/) do
+    #Espera 2 segundos para carregar a caixa de entrada e então clicar em escrever
+    sleep 2
+    element = @browser.find_element(:id, ":yg")
+    element.click
+
+end
+
+E (/^preencher destinatário, assunto e corpo do e-mail$/) do
+    #Preenchendo destinatário
+    destino = @browser.find_element(:name, "to")
+    destino.send_keys("teste@email.com.br")
+
+    #Preenchendo o assunto
+    assunto = @browser.find_element(:name, "subjectbox")
+    assunto.send_keys("Testando Cucumber")
+
+    #Preenchendo o corpo da mensagem
+    msg = @browser.find_element(:css, ".editable")
+    msg.send_keys("Testando o Cucumber e usando Ruby para isso.")
+end
+    
+Entao (/^deve exibir uma mensagem de enviado ao enviar o e-mail$/) do
+    #Clica em enviar
+    @browser.find_element(:css, ".editable").send_keys [:control, :enter]
+    
 end
